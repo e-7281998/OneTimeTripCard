49ef37826b145ae87f8dc821c9a006288c19500d
junbeomSon <junbeom247@gmail.com>
2023-05-30 17:35:12 +0900

    first commit

---
+++ spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/OttcOneTimeTripCardApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.OneTimeTripCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttcOneTimeTripCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttcOneTimeTripCardApplication.class, args);
+	}
+
+}
---
+++ spring boot/OneTimeTripCard/src/test/java/com/shinhan/OneTimeTripCard/OttcOneTimeTripCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.OneTimeTripCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttcOneTimeTripCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
