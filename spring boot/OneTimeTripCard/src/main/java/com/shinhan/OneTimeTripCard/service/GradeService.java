d65466f1f03d2285be36c86a94104052d55f53c3
junbeomSon <junbeom247@gmail.com>
2023-06-12 14:53:00 +0900

    [Spring Boot] feat : 모든 grade 정보 select

--- spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/service/GradeService.java
+++ spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/service/GradeService.java
@@ -1,5 +1,7 @@
 package com.shinhan.OneTimeTripCard.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.shinhan.OneTimeTripCard.repository.GradeRepository;
@@ -15,5 +17,9 @@ public class GradeService {
 	
 	public Grade findById(Long id) {
 		return gradeRepository.findById(id).orElse(null);
+	}
+	
+	public List<Grade> getAllGrades() {
+		return (List<Grade>) gradeRepository.findAll();
 	}
 }
