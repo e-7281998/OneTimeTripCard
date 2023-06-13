438ca61b5377dfb73b0f6a07c7114abdf6f9bc2f
e-7281998 <jejjcs43@naver.com>
2023-06-13 16:21:19 +0900

    [react] feat : course, node, store 데이터 불러오기 / map생성 및 course 표시

--- react_app/src/components/Trip/Map.js
+++ react_app/src/components/Trip/Map.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "assets/css/trip.css";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
+import Course from "./Course";
+const { kakao } = window;
 
 function Map(props) {
-  return <div>Map 컴포넌트</div>;
+  const [course, setCourse] = useState([]);
+  const [node, setNode] = useState([]);
+  const [store, setStore] = useState([]);
+
+  async function getCourse() {
+    const courses = await axios.get("/trip/course");
+    setCourse(courses.data);
+    const nodes = await axios.get("/trip/node");
+    setNode(nodes.data);
+    const stores = await axios.get("/trip/store");
+    setStore(stores.data);
+  }
+
+  useEffect(() => {
+    getCourse();
+  }, []);
+
+  return (
+    <Container>
+      <Course kakao={kakao} course={course} />
+    </Container>
+  );
 }
 
 export default Map;
