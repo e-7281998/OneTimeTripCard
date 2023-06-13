5eb0fbb52b8b03858bfdae6da19781b7363a5509
eho <psj9258@gmail.com>
2023-06-13 17:33:20 +0900

    [Spring Boot] fix : login 기능 연결 성공

--- react_app/src/views/Login/Login.js
+++ react_app/src/views/Login/Login.js
@@ -59,33 +59,27 @@ function Login(props) {
     axios({
       method: "post",
       url: "/login",
-      data: { email: "email", password: "password" },
+      data: { email: email, password: password },
     })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error.response.data);
-      })
       .then((res) => {
         console.log(res);
-        console.log("res.data.email :: ", res.data.email);
+        console.log("res.data.userId :: ", res.data.email);
         console.log("res.data.msg :: ", res.data.msg);
         if (res.data.email === undefined) {
-          // id 일치하지 않는 경우 email = undefined, msg = '입력하신 email이 일치하지 않습니다.'
+          // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
           console.log("======================", res.data.msg);
-          alert("입력하신 email이 일치하지 않습니다.");
+          alert("입력하신 id 가 일치하지 않습니다.");
         } else if (res.data.email === null) {
-          // id는 있지만, pw 는 다른 경우 email = null , msg = undefined
+          // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
           console.log(
             "======================",
             "입력하신 비밀번호 가 일치하지 않습니다."
           );
           alert("입력하신 비밀번호 가 일치하지 않습니다.");
         } else if (res.data.email === email) {
-          // email, pw 모두 일치 email = email1, msg = undefined
+          // id, pw 모두 일치 userId = userId1, msg = undefined
           console.log("======================", "로그인 성공");
-          sessionStorage.setItem("email", email); // sessionStorage에 id를 user_id라는 key 값으로 저장
+          sessionStorage.setItem("user_id", email); // sessionStorage에 id를 user_id라는 key 값으로 저장
           sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
         }
         // 작업 완료 되면 페이지 이동(새로고침)
