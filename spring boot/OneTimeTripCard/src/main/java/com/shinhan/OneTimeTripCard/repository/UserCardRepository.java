7be388a650c5615d2b36b3a2d4b5abc86b05fce8
junbeomSon <junbeom247@gmail.com>
2023-06-14 12:35:48 +0900

    [Spring Boot] feat : user Id 기반 카드 구매 목록 요청 구현

--- spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/repository/UserCardRepository.java
+++ spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/repository/UserCardRepository.java
@@ -1,9 +1,12 @@
 package com.shinhan.OneTimeTripCard.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.shinhan.OneTimeTripCard.vo.UserCard;
 
 public interface UserCardRepository extends CrudRepository<UserCard, Long>{
-
+	
+	public List<UserCard> findByUser_Id(Long userId);
 }
--- spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/service/UserCardService.java
+++ spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/service/UserCardService.java
@@ -1,5 +1,7 @@
 package com.shinhan.OneTimeTripCard.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.shinhan.OneTimeTripCard.repository.UserCardRepository;
@@ -15,5 +17,9 @@ public class UserCardService {
 	
 	public UserCard save(UserCard userCard) {
 		return userCardRepository.save(userCard);
+	}
+	
+	public List<UserCard> findByUser_Id(Long userId) {
+		return userCardRepository.findByUser_Id(userId);
 	}
 }
