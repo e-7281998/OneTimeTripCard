56f810903d62df738512b660a071a87fe59326c5
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-06-18 15:35:06 +0900

    [Spring Boot]  feat : 한단계 높은 등급 찾기

--- spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/repository/GradeRepository.java
+++ spring boot/OneTimeTripCard/src/main/java/com/shinhan/OneTimeTripCard/repository/GradeRepository.java
@@ -1,9 +1,11 @@
 package com.shinhan.OneTimeTripCard.repository;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import com.shinhan.OneTimeTripCard.vo.Grade;
 
 public interface GradeRepository extends CrudRepository<Grade, Long>{
-	
+
+    public List<Grade> findAllOrderByPrice();
 }
