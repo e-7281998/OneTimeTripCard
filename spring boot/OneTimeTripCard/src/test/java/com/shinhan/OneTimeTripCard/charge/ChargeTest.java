29021f434ec06a269d793bffe5bc7927416e66ac
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2023-06-18 16:58:37 +0900

    [Spring Boot] test : 등급 업데이트 테스트

--- spring boot/OneTimeTripCard/src/test/java/com/shinhan/OneTimeTripCard/charge/ChargeTest.java
+++ spring boot/OneTimeTripCard/src/test/java/com/shinhan/OneTimeTripCard/charge/ChargeTest.java
@@ -1,9 +1,12 @@
 package com.shinhan.OneTimeTripCard.charge;
 
 import com.shinhan.OneTimeTripCard.service.ChargeService;
+import com.shinhan.OneTimeTripCard.service.GradeService;
 import com.shinhan.OneTimeTripCard.service.UserCardService;
 import com.shinhan.OneTimeTripCard.vo.Charge;
+import com.shinhan.OneTimeTripCard.vo.Grade;
 import com.shinhan.OneTimeTripCard.vo.UserCard;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,9 @@ public class ChargeTest {
 
     @Autowired
     UserCardService userCardService;
+
+    @Autowired
+    GradeService gradeService;
 
     @Test
     void chargeTest() {
@@ -44,5 +50,44 @@ public class ChargeTest {
         Long userCardId = 76L;
         List<Charge> charges = chargeService.getChargeHistoryByMonth(userCardId, 2023, 6);
         Assertions.assertThat(charges.size()).isEqualTo(3);
+    }
+
+    @Test
+    void upgradeTest() {
+        Long userCardId = 78L;
+        int chargeMoney = 500000;
+        Charge charge = new Charge();
+        charge.setCurrency("dollar");
+        charge.setAmountWon(chargeMoney);
+        charge.setRate(1267.29);
+        charge.setAmount((double) charge.getAmountWon() / charge.getRate());
+
+        UserCard userCard = userCardService.findById(userCardId);
+        Grade grade = userCard.getGrade();
+        charge.setUserCard(userCard);
+
+        chargeService.charge(charge);
+        Grade updatedGrade = userCard.getGrade();
+        Assertions.assertThat(gradeService.getNextGrade(grade.getGradeName()).getGradeName()).isEqualTo(updatedGrade.getGradeName());
+    }
+
+    @Test
+    void notUpgradeTest() {
+        Long userCardId = 76L;
+        UserCard userCard = userCardService.findById(userCardId);
+
+        int chargeMoney = 10000;
+        Charge charge = new Charge();
+        charge.setCurrency("dollar");
+        charge.setAmountWon(chargeMoney);
+        charge.setRate(1267.29);
+        charge.setAmount((double) charge.getAmountWon() / charge.getRate());
+
+        String beforeGradeName = userCard.getGrade().getGradeName();
+        charge.setUserCard(userCard);
+
+        chargeService.charge(charge);
+        Grade updatedGrade = userCard.getGrade();
+        Assertions.assertThat(beforeGradeName).isEqualTo(updatedGrade.getGradeName());
     }
 }
