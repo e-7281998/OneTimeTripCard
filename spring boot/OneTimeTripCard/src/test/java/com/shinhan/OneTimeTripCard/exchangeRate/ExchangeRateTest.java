3c6e88dd747486dd88e1de273c795a482368a3ea
junbeomSon <junbeom247@gmail.com>
2023-06-19 17:43:10 +0900

    Auto stash before merge of "main" and "origin/main"

--- spring boot/OneTimeTripCard/src/test/java/com/shinhan/OneTimeTripCard/exchangeRate/ExchangeRateTest.java
+++ spring boot/OneTimeTripCard/src/test/java/com/shinhan/OneTimeTripCard/exchangeRate/ExchangeRateTest.java
@@ -2,17 +2,22 @@ package com.shinhan.OneTimeTripCard.exchangeRate;
 
 import java.io.IOException;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.shinhan.OneTimeTripCard.service.ExchangeRateScheduler;
+import com.shinhan.OneTimeTripCard.service.ExchangeRateService;
 
 @SpringBootTest
 public class ExchangeRateTest {
 
 	@Autowired
 	ExchangeRateScheduler exchangeRateScheduler;
+	
+	@Autowired
+	ExchangeRateService exchangeRateService;
 	
 //	@Test
 	void callTheAPI() {
@@ -22,5 +27,20 @@ public class ExchangeRateTest {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
+	}
+	
+	@Test
+	void exchangeRateTest() {
+		String [] names = { "dollar", "euro", "yuan", "yen" };
+		double [] exchangeRateToCharge = new double[names.length];
+		double [] exchangeRateToReturn = new double[names.length];
+		for (int i = 0; i < names.length; ++i) {
+			exchangeRateToCharge[i] = exchangeRateService.getExchangeRate(names[i], true);
+			exchangeRateToReturn[i] = exchangeRateService.getExchangeRate(names[i], false);
+		}
+		for (int i = 0; i < names.length; ++i) {
+			Assertions.assertThat(exchangeRateService.getExchangeRate(names[i], true)).isEqualTo(exchangeRateToCharge[i]);
+			Assertions.assertThat(exchangeRateService.getExchangeRate(names[i], false)).isEqualTo(exchangeRateToReturn[i]);
+		}
 	}
 }
