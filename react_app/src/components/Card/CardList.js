f27344991ba5ba49e111d3f888216645d3f16955
eho <psj9258@gmail.com>
2023-06-26 20:56:29 +0900

    [react]  fix :메인페이지 카드 자동슬라이드

--- react_app/src/components/Card/CardList.js
+++ react_app/src/components/Card/CardList.js
@@ -178,155 +178,149 @@ function CardList(props) {
   return (
     <>
       <main ref={props.ref}>
-        <section className="section section-shaped section-lg">
-          <h1>Card List Area</h1>
-          {/* 카드슬라이드 이미지 */}
-          <InfiniteCarousel
-            breakpoints={[
-              {
-                breakpoint: 500,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                },
+        {/* 카드슬라이드 이미지 */}
+        <InfiniteCarousel
+          breakpoints={[
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
               },
-              {
-                breakpoint: 768,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
               },
-            ]}
-            dots={true}
-            showSides={true}
-            // 투명도
-            sidesOpacity={0.3}
-            // 그림 사이즈 (9가제일 작음)
-            sideSize={0.9}
-            slidesToScroll={1}
-            slidesToShow={1}
-            scrollOnDevice={true}
-          >
-            <div>
-              <img alt="" src={require("assets/img/card/cardImg1.png")} />
-            </div>
-            <div>
-              <img alt="" src={require("assets/img/card/1.png")} />
-            </div>
-            <div>
-              <img alt="" src={require("assets/img/card/2.png")} />
-            </div>
-            <div>
-              <img alt="" src={require("assets/img/card/3.png")} />
-            </div>
-          </InfiniteCarousel>
-          <Container fluid>
-            <Row className="justify-content-center">
-              {title.map((item, index) => (
-                <Col key={index}>{item}</Col>
-              ))}
-            </Row>
-            {userCards.map((userCard, index) => (
-              <Row
-                key={index}
-                onClick={clickCard}
-                value={JSON.stringify(userCard)}
-              >
-                <Col>{userCard.nickName}</Col>
-                <Col>{userCard.card?.cardNo}</Col>
-                <Col>{userCard.card?.cardDesign.cardName}</Col>
-                <Col>{userCard.createdAt}</Col>
-                <Col>{userCard.grade?.gradeName}</Col>
-                <Col>{userCard.isDefault ? "Yes" : "No"}</Col>
+            },
+          ]}
+          dots={true}
+          showSides={true}
+          // 투명도
+          sidesOpacity={0.3}
+          // 그림 사이즈 (9가제일 작음)
+          sideSize={0.9}
+          slidesToScroll={1}
+          slidesToShow={1}
+          scrollOnDevice={true}
+        >
+          <div>
+            <img alt="" src={require("assets/img/card/1.png")} />
+          </div>
+          <div>
+            <img alt="" src={require("assets/img/card/2.png")} />
+          </div>
+          <div>
+            <img alt="" src={require("assets/img/card/3.png")} />
+          </div>
+        </InfiniteCarousel>
+        <Container fluid>
+          <Row className="justify-content-center">
+            {title.map((item, index) => (
+              <Col key={index}>{item}</Col>
+            ))}
+          </Row>
+          {userCards.map((userCard, index) => (
+            <Row
+              key={index}
+              onClick={clickCard}
+              value={JSON.stringify(userCard)}
+            >
+              <Col>{userCard.nickName}</Col>
+              <Col>{userCard.card?.cardNo}</Col>
+              <Col>{userCard.card?.cardDesign.cardName}</Col>
+              <Col>{userCard.createdAt}</Col>
+              <Col>{userCard.grade?.gradeName}</Col>
+              <Col>{userCard.isDefault ? "Yes" : "No"}</Col>
 
+              <Col>
+                <Button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (location.pathname === "/travelCard") {
+                      navigate(`/travelCard/history/${userCard.id}`, {
+                        state: { userCard: userCard },
+                      });
+                    } else {
+                      navigate(`/card/history/${userCard.id}`, {
+                        state: { userCard: userCard },
+                      });
+                    }
+                  }}
+                >
+                  사용내역
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  disabled={userCard.isDefault ? "disabled" : ""}
+                  value={userCard.id}
+                  nick={userCard.nickName}
+                  onClick={onDelete}
+                >
+                  삭제하기
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  disabled={userCard.balance === 0 ? "disabled" : ""}
+                  value={userCard.id}
+                  onClick={refund}
+                >
+                  환불하기
+                </Button>
+              </Col>
+            </Row>
+          ))}
+        </Container>
+        <Modal show={show} onHide={handleClose} size="lg">
+          <Modal.Header>
+            <Modal.Title>카드 등록</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row>
+                <Col>카드 번호</Col>
                 <Col>
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      if (location.pathname === "/travelCard") {
-                        navigate(`/travelCard/history/${userCard.id}`, {
-                          state: { userCard: userCard },
-                        });
-                      } else {
-                        navigate(`/card/history/${userCard.id}`, {
-                          state: { userCard: userCard },
-                        });
-                      }
-                    }}
-                  >
-                    사용내역
-                  </Button>
-                </Col>
-                <Col>
-                  <Button
-                    disabled={userCard.isDefault ? "disabled" : ""}
-                    value={userCard.id}
-                    nick={userCard.nickName}
-                    onClick={onDelete}
-                  >
-                    삭제하기
-                  </Button>
-                </Col>
-                <Col>
-                  <Button
-                    disabled={userCard.balance === 0 ? "disabled" : ""}
-                    value={userCard.id}
-                    onClick={refund}
-                  >
-                    환불하기
-                  </Button>
+                  <Form.Control
+                    placeholder="카드 번호"
+                    onChange={getInput}
+                    name="cardNo"
+                  />
                 </Col>
               </Row>
-            ))}
-          </Container>
-          <Modal show={show} onHide={handleClose} size="lg">
-            <Modal.Header>
-              <Modal.Title>카드 등록</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Container>
-                <Row>
-                  <Col>카드 번호</Col>
-                  <Col>
-                    <Form.Control
-                      placeholder="카드 번호"
-                      onChange={getInput}
-                      name="cardNo"
-                    />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>별칭</Col>
-                  <Col>
-                    <Form.Control
-                      placeholder="별칭"
-                      onChange={getInput}
-                      name="nickName"
-                    />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Form.Check
-                      checked={registerInput["isDefault"]}
-                      onChange={checkHandler}
-                    />
-                  </Col>
-                  <Col>기본카드</Col>
-                </Row>
-              </Container>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                취소
-              </Button>
-              <Button variant="primary" onClick={register}>
-                등록
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </section>
+              <Row>
+                <Col>별칭</Col>
+                <Col>
+                  <Form.Control
+                    placeholder="별칭"
+                    onChange={getInput}
+                    name="nickName"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Check
+                    checked={registerInput["isDefault"]}
+                    onChange={checkHandler}
+                  />
+                </Col>
+                <Col>기본카드</Col>
+              </Row>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              취소
+            </Button>
+            <Button variant="primary" onClick={register}>
+              등록
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </main>
     </>
   );
